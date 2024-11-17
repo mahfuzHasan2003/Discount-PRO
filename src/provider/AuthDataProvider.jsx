915eaedb35abf23896dd564ebebd6c51a3220e91
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2024-11-17 20:25:03 +0600

    initial commit

---
+++ src/provider/AuthDataProvider.jsx
@@ -0,0 +1,5 @@
+const AuthDataProvider = () => {
+   return <div></div>;
+};
+
+export default AuthDataProvider;
---
+++ src/provider/CouponDataProvider.jsx
@@ -0,0 +1,5 @@
+const CouponDataProvider = () => {
+   return <div></div>;
+};
+
+export default CouponDataProvider;
